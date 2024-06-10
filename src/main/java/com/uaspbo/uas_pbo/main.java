32e790f11d5a886ea83241350cfbe0128a2534bc
HarHamz <harryhh246@gmail.com>
2024-06-11 01:06:46 +0700

    added footer

--- src/main/java/com/uaspbo/uas_pbo/main.java
+++ src/main/java/com/uaspbo/uas_pbo/main.java
@@ -3,9 +3,11 @@ package com.uaspbo.uas_pbo;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class main extends Application {
     @Override
@@ -13,6 +15,7 @@ public class main extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(main.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Jong JavaFX");
+        stage.getIcons().add(new Image(Objects.requireNonNull(main.class.getResourceAsStream("USU.png"))));
         stage.setScene(scene);
         stage.show();
     }
