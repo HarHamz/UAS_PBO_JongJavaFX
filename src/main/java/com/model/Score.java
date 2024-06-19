6b80e1b957b130ccd2a2912c004e21c43be918cc
Healm <116085297+Agent-Healm@users.noreply.github.com>
2024-06-19 14:51:27 +0700

    final changes to Main.java and fixed description issue

---
+++ src/main/java/com/model/Score.java
@@ -0,0 +1,54 @@
+package com.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Column;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "score")
+@Inheritance(strategy = InheritanceType.JOINED)
+public class Score {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "nilai", nullable = false)
+    private int value;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Kelas kelas;
+
+    @ManyToOne
+    private Student student;
+
+    public Score() {}
+
+    public Score(int value, Kelas kelas, Student student) {
+        this.value = value;
+        this.kelas = kelas;
+        this.student = student;
+    }
+
+    public Score(int value, Kelas kelas) {
+        this.value = value;
+        this.kelas = kelas;
+    }
+
+    public int getId() { return id; }
+    public int getValue() { return value; }
+    public Kelas getKelas() { return kelas; }
+    public Student getStudent() { return student; }
+
+    public void setId(int id) { this.id = id; }
+    public void setValue(int value) { this.value = value; }
+    public void setKelas(Kelas kelas) { this.kelas = kelas; }
+    public void setStudent(Student student) { this.student = student; }
+
+}
